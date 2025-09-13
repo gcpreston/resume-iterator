d97a5a7b71bf2b7eaefe4432d78da49b6cc4d5fb
Graham Preston <graham.preston@gmail.com>
2025-09-14 01:12:26 +0200

    Consistent styling

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['@modelcontextprotocol/sdk']
-  }
-}
+    serverComponentsExternalPackages: ["@modelcontextprotocol/sdk"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
